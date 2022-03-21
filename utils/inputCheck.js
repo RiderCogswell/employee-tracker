694e6a5efdb19b8aa920fa9063b5af69ac94ad34
Ridercogswell <ridercogswell@gmail.com>
2022-03-21 16:16:56 -0700

    add input check function

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,4 +1,19 @@
 // checks to make sure that an object has the required properties
 module.exports = function(obj, ...props) {
+    const errors = [];
 
+    props.forEach((prop) => {
+        // if property is blank or doesnt exist, add to errors array
+        if (obj[prop] === undefined || obj[prop] === '') {
+            errors.push(`No ${prop} specified.`);
+        }
+    })
+
+    if (errors.length) {
+        return {
+            error: errors.join(' ')
+        };
+    }
+    
+    return null;
 }
